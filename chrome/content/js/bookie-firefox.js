d3a7efd317e0b85bf4cc310b0581873192d0e0c3
Adam J. Lincoln <adamjlincoln@gmail.com>
2011-04-12 23:35:32 -0400

    Now the preferences for the extension can be set.  Next, figure out UI for the extension and get JS going.

--- chrome/content/js/bookie-firefox.js
+++ chrome/content/js/bookie-firefox.js
@@ -7,6 +7,53 @@ window.addEventListener("load", function(event){
         )
     );
 
-    
+    var Bookie = {
+        prefs: null,
+        serverUrl: "",
+        apiKey: "",
+
+        startup: function()
+        {
+             this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
+                 .getService(Components.interfaces.nsIPrefService)
+                 .getBranch("bookie.");
+             this.prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
+             this.prefs.addObserver("", this, false);
+
+             this.serverUrl = this.prefs.getCharPref("server");
+             this.apiKey = this.prefs.getCharPref("apikey");
+
+             //this.refreshInformation();
+             //window.setInterval(this.refreshInformation, 10*60*1000);
+
+        },    
+
+        shutdown: function()
+        {
+            this.prefs.removeObserver("", this);
+        },
+
+        observe: function(subject, topic, data)
+        {
+            if (topic != "nsPref:changed")
+            {
+                return;
+            }
+ 
+            switch(data)
+            {
+                case "server":
+                    this.serverUrl = this.prefs.getCharPref("server");
+                    break;
+                case "apikey":
+                    this.apiKey = this.prefs.getCharPref("apikey");
+                    break;
+             }
+        },
+
+    }
+
+    window.addEventListener("load", function(e) { Bookie.startup(); }, false);
+    window.addEventListener("unload", function(e) { Bookie.shutdown(); }, false);
 
 }, false);
