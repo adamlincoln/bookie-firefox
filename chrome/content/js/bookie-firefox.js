786a827f1daa3d5fd6615b55acef39546ce2ac4b
Adam J. Lincoln <adamjlincoln@gmail.com>
2011-04-21 16:03:47 -0400

    Looks like all is ok now.  bookie-core is the same as the one in the Chrome extension. bookie.xul brings in bookie-firefox, which then brings in jquery.min.js and bookie-core using the Firefox loadSubScript magic.  Then jQuery.noConflict() is used to release the global jQuery and $ names and assign them to bookie object variables.  Now we can build on this.

--- chrome/content/js/bookie-firefox.js
+++ chrome/content/js/bookie-firefox.js
@@ -1,87 +1,89 @@
-//This wrapper is used in bookie-core.js.  I can't tell if it's needed here too.
-//window.addEventListener("load", function(event){
-    //(function(loader){
-        //loader.loadSubScript("chrome://bookie/content/js/jquery.min.js");
-    //})(
-        //Components.classes["@mozilla.org/moz/jssubscript-loader;1"].getService(
-            //Components.interfaces.mozIJSSubScriptLoader
-        //)
-    //);
+// This is an attempt to make http://meherranjan.com/blog/a-guide-to-using-jquery-inside-firefox-extension/
+// work (approach 2).  This is used with bookie-core.js NOT included in the .xul file.  It looks like this works ok
+// on its own, but I can't work out how to use the jQuery bit you get back to include useful stuff from
+// bookie-core.js.
+var bookie = (function(module) {
+    module.init = (function(context) {
+        var loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"].getService(Components.interfaces.mozIJSSubScriptLoader);
+        //loader.loadSubScript("chrome://bookie/content/js/jquery.min.js", context);
+        loader.loadSubScript("chrome://bookie/content/js/jquery.min.js");
+        loader.loadSubScript("chrome://bookie/content/js/bookie-core.js");
 
-    // Trying this doesn't work - but I don't know if it should.  Seems like bookie isn't visible.
-    //bookie.aConsoleService.logStringMessage('foo');
-    //(function (module, $) {
-        // Trying this doesn't work either.
-        //module.aConsoleService.logStringMessage('bar');
+        this.$ = this.jQuery = jQuery.noConflict(true);
 
-    //})(bookie || {}, jQuery);
+        module.consoleService = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
+        // This works here just fine.  And if later you use the aConsoleService attribute, it works fine.
+        module.consoleService.logStringMessage('Adding to bookie in bookie-firefox');
 
-    // This is an attempt to make http://meherranjan.com/blog/a-guide-to-using-jquery-inside-firefox-extension/
-    // work (approach 2).  This is used with bookie-core.js NOT included in the .xul file.  It looks like this works ok
-    // on its own, but I can't work out how to use the jQuery bit you get back to include useful stuff from
-    // bookie-core.js.
-    var BookiejQuery = {
-        loadjQuery: function(context) {
-            var loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"].getService(Components.interfaces.mozIJSSubScriptLoader);
-            loader.loadSubScript("chrome://bookie/content/js/jquery.min.js", context);
+        //(function (module, $) {
+            //module.test = 'bar';
+        //})(this.bookie || {}, jQuery);
+        //return this.bookie;
 
-            var jQuery = window.jQuery.noConflict(true);
-            if(typeof(jQuery.fn._init) == 'undefined') {
-                jQuery.fn._init = jQuery.fn.init;
-            }
-            this.jQuery = jQuery;
-        },
+        // Here's some crap from that link above that I think we don't need right now.
+        //if(typeof(jQuery.fn._init) == 'undefined') {
+            //jQuery.fn._init = jQuery.fn.init;
+        //}
+        //this.jQuery = jQuery;
+    });
+
+    module.test = function() {
+        alert(this.jQuery);
     };
 
-    var bookie = (function (module, $) {
-        // Just a placeholder for some testing.
-    })(bookie || {}, jQuery);
+    return module;
+})(bookie || {});
 
-    // Here's some stuff that makes preferences behave in firefox.  Commented out while working on jQuery stuff.
-    /*var Bookie = {
-        prefs: null,
-        serverUrl: "",
-        apiKey: "",
+//for (i in window) {
+    //if (i.slice(0,1) == "B") {
+        //aConsoleService.logStringMessage(i + ': ' + window[i]);
+    //}
+//}
 
-        startup: function()
+// Here's some stuff that makes preferences behave in firefox.  Commented out while working on jQuery stuff.
+/*var Bookie = {
+    prefs: null,
+    serverUrl: "",
+    apiKey: "",
+
+    startup: function()
+    {
+         this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
+             .getService(Components.interfaces.nsIPrefService)
+             .getBranch("bookie.");
+         this.prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
+         this.prefs.addObserver("", this, false);
+
+         this.serverUrl = this.prefs.getCharPref("server");
+         this.apiKey = this.prefs.getCharPref("apikey");
+
+    },    
+
+    shutdown: function()
+    {
+        this.prefs.removeObserver("", this);
+    },
+
+    observe: function(subject, topic, data)
+    {
+        if (topic != "nsPref:changed")
         {
-             this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
-                 .getService(Components.interfaces.nsIPrefService)
-                 .getBranch("bookie.");
-             this.prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
-             this.prefs.addObserver("", this, false);
+            return;
+        }
 
-             this.serverUrl = this.prefs.getCharPref("server");
-             this.apiKey = this.prefs.getCharPref("apikey");
-
-        },    
-
-        shutdown: function()
+        switch(data)
         {
-            this.prefs.removeObserver("", this);
-        },
+            case "server":
+                this.serverUrl = this.prefs.getCharPref("server");
+                break;
+            case "apikey":
+                this.apiKey = this.prefs.getCharPref("apikey");
+                break;
+         }
+    },
 
-        observe: function(subject, topic, data)
-        {
-            if (topic != "nsPref:changed")
-            {
-                return;
-            }
- 
-            switch(data)
-            {
-                case "server":
-                    this.serverUrl = this.prefs.getCharPref("server");
-                    break;
-                case "apikey":
-                    this.apiKey = this.prefs.getCharPref("apikey");
-                    break;
-             }
-        },
+}
 
-    }
+window.addEventListener("load", function(e) { Bookie.startup(); }, false);
+window.addEventListener("unload", function(e) { Bookie.shutdown(); }, false);*/
 
-    window.addEventListener("load", function(e) { Bookie.startup(); }, false);
-    window.addEventListener("unload", function(e) { Bookie.shutdown(); }, false);*/
-
-//}, false);
